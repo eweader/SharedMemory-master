93f5955dd543cd43d362b4ac6cd08470fb0f3a4a
eweader <eweader@users.noreply.github.com>
2017-09-26 21:41:24 -0400

    adding experiment

---
+++ Experiments/PriceConsumerConsole/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriceConsumerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mname = "PovEURUSDm5" ;
+            using (var consumer
+    = new SharedMemory.SharedArray<int>(mname))
+            {
+                ;
+
+                Console.WriteLine(consumer[0]);
+                Console.WriteLine(consumer[consumer.Length - 1]);
+            }
+        }
+    }
+    
+}
